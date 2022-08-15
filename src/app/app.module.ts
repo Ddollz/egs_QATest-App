d9a947652c556dfe4dc282417610a27879920f87
roycesguerra <89351111+roycesguerra@users.noreply.github.com>
2022-08-15 10:24:21 +0800

    projects/repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatSelectModule } from '@angular/material/select';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatRadioModule } from '@angular/material/radio';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,19 +15,16 @@ import { SideBarComponent } from './side-bar/side-bar.component';
 import { EgsWorkspaceComponent } from './egs-workspace/egs-workspace.component';
 import { UserComponent } from './egs-workspace/user/user.component';
 import { RolesComponent } from './egs-workspace/roles/roles.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TestRunComponent } from './egs-project/projects/test-run/test-run.component';
+import { RepositoryComponent } from './egs-project/projects/repository/repository.component';
 
 //Link Routes
 const appRoute: Routes = [
   { path: '', redirectTo: 'projects', pathMatch: "full" },
   { path: 'projects', component: ProjectsComponent },
-  { path: 'projects/run', component: TestRunComponent },
-  //Uncomment this when project is working
-  // { path: 'projects/run/:project', component: TestRunComponent },
+  { path: 'projects/repository', component: RepositoryComponent },
   { path: 'workspace', component: EgsWorkspaceComponent },
   { path: 'workspace/user', component: UserComponent },
-  { path: 'workspace/roles', component: RolesComponent },
+  { path: 'workspace/user/:id:test', component: UserComponent },
   { path: 'login', component: EgsLoginComponent },
   { path: 'register', component: EgsRegistrationComponent },
 ]
@@ -48,7 +40,7 @@ const appRoute: Routes = [
     EgsWorkspaceComponent,
     UserComponent,
     RolesComponent,
-    TestRunComponent
+    RepositoryComponent
   ],
   imports: [
     BrowserModule,
@@ -56,14 +48,7 @@ const appRoute: Routes = [
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatSelectModule,
-    MatCheckboxModule,
-    MatRadioModule,
-    RouterModule.forRoot(appRoute),
-    BrowserAnimationsModule
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/egs-workspace/roles/roles.component.ts
+++ src/app/egs-workspace/roles/roles.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { role, user } from '../../models/workspace/workspace.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-roles',
@@ -10,18 +6,10 @@ import { role, user } from '../../models/workspace/workspace.model';
   styleUrls: ['./roles.component.css']
 })
 export class RolesComponent implements OnInit {
-  roles: role[] = [
-    { value: 'Administrator',description:"Admin Description", checked: true, users : 2 },
-    { value: 'Editor',description:"Editor Description", checked: true, users : 1 },
-    { value: 'Guest',description:"Guest  Description", checked: true, users : 5 },
-  ];
-  displayedColumns: string[] = ['value','users','setting'];
-  dataSource = new MatTableDataSource<role>(this.roles);
-  @ViewChild(MatSort) sort: any = MatSort;
-  @ViewChild(MatPaginator) paginator: any = MatPaginator;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
 }
