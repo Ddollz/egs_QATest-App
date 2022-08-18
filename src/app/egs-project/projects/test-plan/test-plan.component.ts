53080a948f38d016e5a9c023f9cda128c71dd8d5
lncervr16 <lnce.rvr16@gmail.com>
2022-08-18 11:46:02 +0800

    Test-Plan & Create-Plan

---
+++ src/app/egs-project/projects/test-plan/test-plan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-plan',
+  templateUrl: './test-plan.component.html',
+  styleUrls: ['./test-plan.component.css']
+})
+export class TestPlanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
