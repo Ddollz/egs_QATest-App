c0430f203980a1f1e0f6e7c54ba7afe3cf449d8e
Ddollz <44191473+Ddollz@users.noreply.github.com>
2022-10-07 16:43:31 +0800

    Revert "First try theme mode"
    This reverts commit e49aeb9443e908fcf807b189b5c48f6eb083b5a9.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
-import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { filter, Observable } from 'rxjs';
 import { ApiService } from './services/api.service';
 import { Router, NavigationStart, NavigationEnd } from '@angular/router';
-import { DOCUMENT } from '@angular/common';
-
-export type Theme = 'light-theme' | 'dark-theme';
 
 @Component({
   selector: 'app-root',
@@ -13,23 +10,17 @@ export type Theme = 'light-theme' | 'dark-theme';
 })
 export class AppComponent implements OnInit {
 
-  theme: Theme = 'light-theme';
-
   roleList$!: Observable<any[]>;
 
   showHead: boolean = false;
   showSide: boolean = false;
 
-  constructor(
-    @Inject(DOCUMENT) private document: Document,
-    private renderer: Renderer2,
-    private api: ApiService, public router: Router
-  ) {
+  constructor(private api: ApiService, public router: Router) {
     //theme toggle
     document.body.classList.toggle("light-theme");
   }
   ngOnInit(): void {
-    this.initializeTheme();
+
     //Sample Post Methods
     // this.api.UniCall(
     //   {
@@ -50,8 +41,4 @@ export class AppComponent implements OnInit {
     //     complete() { console.log('Finished sequence'); }
     //   })
   }
-  themeSwitch() {
-    this.document.body.classList.replace(this.theme, this.theme === 'light-theme' ? (this.theme = 'dark-theme') : (this.theme = 'light-theme'))
-  }
-  initializeTheme = (): void => this.renderer.addClass(this.document.body, this.theme);
 }
