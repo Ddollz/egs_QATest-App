bb85517ed6b167d325958a847d06ade2f7774df6
Ddollz <44191473+Ddollz@users.noreply.github.com>
2022-10-07 16:50:19 +0800

    Revert "First try theme mode"
    This reverts commit e49aeb9443e908fcf807b189b5c48f6eb083b5a9.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,17 +6,10 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  @Output() toggleMode = new EventEmitter<boolean>();
-  theme: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-  toggle(value: boolean) {
-    this.theme = !value;
-    console.log(this.theme)
-    this.toggleMode.emit(value);
   }
 
 }
