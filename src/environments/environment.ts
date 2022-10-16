411dea504c9766430f267cdcfccb2ebf45757759
Ddollz <44191473+Ddollz@users.noreply.github.com>
2022-10-16 18:49:19 +0800

    Revert "Progress perSuities"
    This reverts commit e915d0c40166492d6102d5fb8bc722f3e97a5fab.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // api_url: 'http://192.168.1.126:8082/api/', //!This is for deployed web api
-  api_url: 'https://localhost:7020/api/',    //!This is for localhost webapi
+  api_url: 'http://192.168.1.126:8082/api/', //!This is for deployed web api
+  // api_url: 'https://localhost:7020/api/',    //!This is for localhost webapi
   unicall_url: 'UniCallController_SQLServer/UniCall',
   uniattach_url: 'UniCallController_SQLServer/UniAttachment',
   auth_url: 'Auth/',
